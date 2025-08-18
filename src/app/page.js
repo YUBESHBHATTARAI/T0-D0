6465e07022451ca597606770b7ee8b429a46525f
Yubesh <yubeshbhattarai@gmail.com>
2025-08-18 13:56:44 +0545

    css Added and the site is completed

--- src/app/page.js
+++ src/app/page.js
@@ -32,31 +32,33 @@ export default function Home() {
       <div>
         <div>
           <h1 className=" text-center">to do list</h1>
-          <div className="flex flex-col items-center justify-center ring-2 ring-white w-[50vw] mx-auto  ">
-            <input className=" flex "
+          <div className="flex flex-col items-center justify-center ring-2 ring-white rounded-xl  w-[96vw] md:w-[60vw] lg:w-[45vw] xl:w-[35vw] mx-auto gap-1   ">
+            <input className=" px-6 py-2 mx-2 my-3 ring-1 ring-gray-300 rounded-md "
               type="text"
-              placeholder="add a new to do "
+              placeholder="Add a new to do " 
               value={search}
               onChange={(e) => setSearch(e.target.value)}
             />
-            <button onClick={addData}>"+"</button>
+            <button className="text-white text-4xl  rounded-md hover:cursor-pointer mb-2  " onClick={addData}><span className="text-2xl font-bold bg-gray-300 text-black p-1 rounded-md  ">+</span></button>
             <ul>
               {data.map((item, value) => (
-                <div key={value} className=" flex  ">
-                  <span>{item}</span>
-                  <div className="flex ">
-                    <button onClick={() => deleteData(item)}>"X"</button>
+                <div key={value} className=" flex justify-between items-center gap-4  mb-4  ">
+                  <span className="text-4xl  rounded-md hover:cursor-pointer font-[Inter] text-white    ">{item}</span>
+                  <div className="flex gap-3 ">
+                    <button className="text-white text-3xl font-thin  rounded-md hover:cursor-pointer  " onClick={() => deleteData(item)}>  <span className="text-2xl font-bold bg-gray-300 text-black p-1 rounded-md  ">&#10005;</span></button>
                     <button
+                    className="text-white text-3xl font-bold  rounded-md hover:cursor-pointer  "
                       onClick={() => moveUp(value)}
                       disabled={value === 0}
                     >
-                      ⬆️
+                     <span className="text-2xl font-bold bg-gray-300 text-black p-1 rounded-md ">&#8593;</span>
                     </button>
                     <button
+                    className="text-white text-3xl font-bold rounded-md hover:cursor-pointer  "
                       onClick={() => moveDown(value)}
                       disabled={value === data.length - 1}
                     >
-                      ⬇️
+                      <span className="text-2xl font-bold bg-gray-300 text-black p-1 rounded-md ">&#8595;</span>
                     </button>
                   </div>
                 </div>
